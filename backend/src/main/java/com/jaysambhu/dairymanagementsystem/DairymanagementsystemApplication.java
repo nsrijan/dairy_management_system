f533d40642ac7af92e225f5597fde864707143bf
srijan <sr12jan@gmail.com>
2025-04-11 07:22:31 +0545

    Initial commit with unified project structure

---
+++ backend/src/main/java/com/jaysambhu/dairymanagementsystem/DairymanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.jaysambhu.dairymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairymanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairymanagementsystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jaysambhu/dairymanagementsystem/DairymanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaysambhu.dairymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairymanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
